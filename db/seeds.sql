72cd695123a1578d959541491bbcaa20f6eaf938
Matt Pengelly <matt.pengelly@edgenuity.com>
2019-05-21 23:07:42 -0700

    getting close

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Colossus', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamberder', false);
